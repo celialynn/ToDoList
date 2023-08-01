7dede5cd10bebaf36e5c60f54cf95012e4bd420c
Celia Schreiber <schreibercelia@gmail.com>
2023-08-01 10:26:36 +0100

    Iteration zero

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -10,17 +10,26 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Text("To Do List")
+                    .font(.system(size: 40))
+                    .fontWeight(.black)
+                Button(action: {
+                    
+                }) {
+                    Text("+")
+                }
+            }
+            Spacer()
+            
         }
         .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
---
+++ ToDoList/ToDoItem.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoItem.swift
+//  ToDoList
+//
+//  Created by scholar on 28/07/2023.
+//
+
+import Foundation
+
+class ToDoItem {
+    var title = ""
+    var isImportant = false
+    
+    init(title: String = "", isImportant: Bool = false) {
+        self.title = title
+        self.isImportant = isImportant
+    }
+}
